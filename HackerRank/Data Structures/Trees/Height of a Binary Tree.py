515cf6afce7a9787354366ac3104a6992ef12a98
direcision <direcision@gmail.com>
2019-02-02 13:33:55 +0900

    Create Height of a Binary Tree.py

---
+++ HackerRank/Data Structures/Trees/Height of a Binary Tree.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+def height(root): 
+    if root == None:
+        return -1
+    return max(height(root.right), height(root.left)) + 1
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
