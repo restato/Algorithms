23d5ba6551369f2f2a969456c6cb08e6c55c4dfa
direcision <direcision@gmail.com>
2019-02-18 21:10:18 +0900

    Create dfs.py

---
+++ Graph/dfs.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+def dfs(graph, start):
+    visited = []
+    stack = [start]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack += graph[n] - set(visited)
+    return visited
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    result = []
+
+    while stack:
+        n, path = stack.pop()
+        if n == goal:
+            result.append(path)
+            # return result
+        else:
+            for m in set(graph[n]) - set(path):
+                stack.append((m, path + [m]))
+    return result
+
+def dfs_recursive(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs_recursive(graph, next, visited)
+    return visited
+
+if __name__ == "__main__":
+    graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])} 
+
+    print (dfs(graph, 'A'))
+    print (dfs_recursive(graph, 'A'))
+    print (dfs_paths(graph, 'A', 'F'))
