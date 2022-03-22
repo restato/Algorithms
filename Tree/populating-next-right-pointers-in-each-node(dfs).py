95984179ef99dc306e082e546c23d6cfbebadadb
direcision <direcision@gmail.com>
2022-03-22 23:54:53 +0900

    Create populating-next-right-pointers-in-each-node(dfs).py

---
+++ Tree/populating-next-right-pointers-in-each-node(dfs).py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root):
+        if not root: return None
+        
+        L, R, N = root.left, root.right, root.next
+        if L:
+            L.next = R
+            if N: R.next = N.left # 🔑
+            self.connect(L)
+            self.connect(R)
+        return root
